a625eebc0cf6698d7fd8661a7e04c2bd384c9701
Adam Švidroň <adam.svidron@pekatvision.com>
2026-01-19 18:58:32 +0100

    Testing CI

---
+++ build.ps1
@@ -0,0 +1,39 @@
+& "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64 -HostArch amd64
+
+Push-Location .\faiss
+
+if (-not $env:CUDA_PATH) {
+    $env:CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9"
+}
+$env:PATH = "$env:CUDA_PATH\bin;$env:PATH"  # $env:CUDA_PATH set by cuda action
+
+# Use MKL from virtual environment
+# $pythonLibrary = "$PSScriptRoot\.venv\Library"
+$pythonLibrary = "$env:pythonLocation\Library"
+
+$env:MKLROOT = $pythonLibrary
+
+# Get PyTorch's lib directory for OpenMP
+$torchLib = python -c "import torch; import os; print(os.path.join(os.path.dirname(torch.__file__), 'lib'))"
+
+cmake -G "Ninja" -B build . `
+    -DCUDAToolkit_ROOT="$env:CUDA_PATH" `
+    -DCMAKE_PREFIX_PATH="$torchLib;$pythonLibrary;$env:CUDA_PATH\lib\cmake" `
+    -DFAISS_ENABLE_GPU=ON `
+    -DFAISS_ENABLE_PYTHON=ON `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DCMAKE_CUDA_ARCHITECTURES="75;86;89;120" `
+    -DBUILD_TESTING=OFF `
+    -DCMAKE_INSTALL_PREFIX=install `
+    -DFAISS_OPT_LEVEL=avx2 `
+    -DBLA_VENDOR=Intel10_64lp `
+    -DCMAKE_CXX_STANDARD=17 `
+    -DCMAKE_CUDA_STANDARD=17 `
+    
+# Build
+cmake --build build --parallel
+
+# Install C++ library
+cmake --install build
+
+Pop-Location
